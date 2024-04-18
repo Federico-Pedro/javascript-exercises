a03d81b77a96fa0c76177f757affc6a815d9a016
Federico-Pedro <125419127+Federico-Pedro@users.noreply.github.com>
2024-04-18 07:56:48 -0300

    Solve exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const char = 'abcdefghijklmnopqrstuvwxyz123456789';
+    newStr = str.toLowerCase()
+                .split('')
+                .filter((character) => char.includes(character))
+                .join('');
+    reverseStr = newStr.split('').reverse().join('');
+    return newStr === reverseStr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((acumulator, current) => {
+        const first = getAge(acumulator.yearOfBirth, acumulator.yearOfDeath);
+        const second = getAge(current.yearOfBirth, current.yearOfDeath);
+        return first < second ? current : acumulator;
+    })
+};
 
+
+const getAge = function(birth, death) {
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
